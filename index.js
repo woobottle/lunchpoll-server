5e4c2603e54270d4bf896632840cb576b794af70
woobottle <dkrnfls@gmail.com>
2020-02-09 22:39:12 +0900

    post 수정

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 var express = require('express');
 var app = express();
-
+app.set('view engine', 'jade');
+app.set('views', './views');
 app.use(express.static('public'));
 //사용자가 get 방식으로 접속한걸 잡기 위해서
-app.get('/', function (req, res) {
+app.post('/', function (req, res) {
   res.send('Hello home page');
 });
 
-app.get('/login', function (req, res) {
-  res.send('Login Please');
+app.get('/form', function (req, res) {
+  res.render('form');
 })
+
+app.post('/form_receiver', function(req, res){
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title + ',' + description)
+});
 
 app.get('/topic', function (req, res) {
   res.send(req.query.id);
 })
 
-app.listen(3000, function () {
-  console.log('Connected 3000 port!');
-});
-
-
 app.post("/actions", function (req, res) {
-  res.status(200).end(); 
+  // res.status(200).end(); 
+  // res.json({ok: true});
   res.send('actions')
   console.log("afdfasdfds");
+});
+
+app.listen(3000, function() {
+  console.log("Connected 3000 port!");
 });
