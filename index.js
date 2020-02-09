9f34871687ec07f9d948563e5d417b189cd45dfc
woobottle <dkrnfls@gmail.com>
2020-02-09 22:49:34 +0900

    post 실험

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var app = express();
+app.unsubscribe(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+
 //사용자가 get 방식으로 접속한걸 잡기 위해서
-app.post('/', function (req, res) {
-  res.send('Hello home page');
+// app.get('/', function (req, res) {
+//   res.send('Hello home page');
+// });
+
+app.post("/", function(req, res, next) {
+  console.log(req.body)
+  res.json(req.body)
+  res.send(req.body);
 });
+
 
 app.get('/form', function (req, res) {
   res.render('form');
 })
 
-app.post('/form_receiver', function(req, res){
-  var title = req.query.title;
-  var description = req.query.description;
-  res.send(title + ',' + description)
+app.post('/form_receiver', function(req, res, next){
+  console.log(req.body)
+  res.json(req.body)
+  // var title = req.query.title;
+  // var description = req.query.description;
+  // res.send(title + ',' + description)
 });
 
 app.get('/topic', function (req, res) {
