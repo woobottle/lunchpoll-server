38771de98a66fda5e9d8e06890f561d5d0ae86da
woobottle <dkrnfls@gmail.com>
2020-02-13 00:46:41 +0900

    f

--- index.js
+++ index.js
@@ -24,44 +24,41 @@ app.post("/send-me-buttons", urlencodedParser, (req, res) => {
 res.status(200).end() // best practice to respond with empty 200 status code
     var reqBody = req.body
     var responseURL = reqBody.response_url
-    if (reqBody.token != YOUR_APP_VERIFICATION_TOKEN){
-        res.status(403).end("Access forbidden")
-    }else{
-        var message = {
-            "text": "This is your first interactive message",
-            "attachments": [
-                {
-                    "text": "Building buttons is easy right?",
-                    "fallback": "Shame... buttons aren't supported in this land",
-                    "callback_id": "button_tutorial",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "yes",
-                            "text": "yes",
-                            "type": "button",
-                            "value": "yes"
-                        },
-                        {
-                            "name": "no",
-                            "text": "no",
-                            "type": "button",
-                            "value": "no"
-                        },
-                        {
-                            "name": "maybe",
-                            "text": "maybe",
-                            "type": "button",
-                            "value": "maybe",
-                            "style": "danger"
-                        }
-                    ]
-                }
-            ]
-        }
-        sendMessageToSlackResponseURL(responseURL, message)
+    var message = {
+        "text": "This is your first interactive message",
+        "attachments": [
+            {
+                "text": "Building buttons is easy right?",
+                "fallback": "Shame... buttons aren't supported in this land",
+                "callback_id": "button_tutorial",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "yes",
+                        "text": "yes",
+                        "type": "button",
+                        "value": "yes"
+                    },
+                    {
+                        "name": "no",
+                        "text": "no",
+                        "type": "button",
+                        "value": "no"
+                    },
+                    {
+                        "name": "maybe",
+                        "text": "maybe",
+                        "type": "button",
+                        "value": "maybe",
+                        "style": "danger"
+                    }
+                ]
+            }
+        ]
     }
+    sendMessageToSlackResponseURL(responseURL, message)
+    
 });
 
 app.get('/form', function (req, res) {
