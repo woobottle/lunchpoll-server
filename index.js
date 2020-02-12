f9479dcf01992bfefccd496b914665455f957130
woobottle <dkrnfls@gmail.com>
2020-02-13 01:06:24 +0900

    F

--- index.js
+++ index.js
@@ -15,42 +15,41 @@ app.use(express.static('public'));
 // });
 
 app.post("/", urlencodedParser, function(req, res) {
-  res.send("fadsfds");
   res.status(200).end()
   var reqBody = req.body;
   var responseURL = reqBody.response_url;
   var message = {
     text: "This is your first interactive message",
-    attachments: [
-      {
-        text: "Building buttons is easy right?",
-        fallback: "Shame... buttons aren't supported in this land",
-        callback_id: "button_tutorial",
-        color: "#3AA3E3",
-        attachment_type: "default",
-        actions: [
-          {
-            name: "yes",
-            text: "yes",
-            type: "button",
-            value: "yes"
-          },
-          {
-            name: "no",
-            text: "no",
-            type: "button",
-            value: "no"
-          },
-          {
-            name: "maybe",
-            text: "maybe",
-            type: "button",
-            value: "maybe",
-            style: "danger"
-          }
-        ]
-      }
-    ],
+    // attachments: [
+    //   {
+    //     text: "Building buttons is easy right?",
+    //     fallback: "Shame... buttons aren't supported in this land",
+    //     callback_id: "button_tutorial",
+    //     color: "#3AA3E3",
+    //     attachment_type: "default",
+    //     actions: [
+    //       {
+    //         name: "yes",
+    //         text: "yes",
+    //         type: "button",
+    //         value: "yes"
+    //       },
+    //       {
+    //         name: "no",
+    //         text: "no",
+    //         type: "button",
+    //         value: "no"
+    //       },
+    //       {
+    //         name: "maybe",
+    //         text: "maybe",
+    //         type: "button",
+    //         value: "maybe",
+    //         style: "danger"
+    //       }
+    //     ]
+    //   }
+    // ],
     blocks: [
       [
         {
