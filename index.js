8df573dfa3cf64c597b5013cf367d4c39db898d9
woobottle <dkrnfls@gmail.com>
2020-02-14 00:06:46 +0900

    fc

--- index.js
+++ index.js
@@ -125,7 +125,9 @@ app.post("/actions", urlencodedParser, (req, res) => {
   res.status(200).end(); // best practice to respond with 200 status
   var actionJSONPayload = JSON.parse(req.body.payload); // parse URL-encoded payload JSON string
   console.log(actionJSONPayload);
+  debugger;
   var message = {
+    response_type: "in_channel",
     text:
       actionJSONPayload.user.name +
       " clicked: " +
