d3283506466c923710e91530df60acac13b3093f
woobottle <dkrnfls@gmail.com>
2020-02-13 23:52:55 +0900

    fc

--- index.js
+++ index.js
@@ -43,13 +43,39 @@ app.post("/", urlencodedParser, function(req, res) {
           text: first
         },
         accessory: {
-          type: "button",
-          text: {
-            type: "plain_text",
-            emoji: true,
-            text: "Vote"
+          "type": "block_actions",
+          "team": {
+            "id": "T0CAG",
+            "domain": "acme-creamery"
           },
-          value: "click_me_123"
+          "user": {
+            "id": "U0CA5",
+            "username": "Amy McGee",
+            "name": "Amy McGee",
+            "team_id": "T3MDE"
+          },
+          "api_app_id": "A0CA5",
+          "token": "Shh_its_a_seekrit",
+          "container": {
+            "type": "message",
+            "text": "The contents of the original message where the action originated"
+          },
+          "trigger_id": "12466734323.1395872398",
+          "response_url": "https://www.postresponsestome.com/T123567/1509734234",
+          "actions": [
+            {
+              "type": "button",
+              "block_id": "qX8",
+              "action_id": "tTSSV",
+              "text": {
+                "type": "plain_text",
+                "text": "Choose",
+                "emoji": true
+              },
+              "value": "click_me_123",
+              "action_ts": "1581605443.101423"
+            }
+          ]
         }
       },
       {
