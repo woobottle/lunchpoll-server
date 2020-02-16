6a8507cd9a31f08aac2c2a213a6a1fa96752533a
woobottle <dkrnfls@gmail.com>
2020-02-16 23:56:37 +0900

    f

--- index.js
+++ index.js
@@ -159,9 +159,13 @@ app.post("/actions", urlencodedParser, (req, res) => {
   res.status(200).end(); // best practice to respond with 200 status
   var actionJSONPayload = JSON.parse(req.body.payload); // parse URL-encoded payload JSON string
   console.log(actionJSONPayload);
+  console.log("------------------")
   console.log(actionJSONPayload.actions[0])
+  console.log("------------------")
   console.log(actionJSONPayload.actions[1])
+  console.log("------------------") 
   console.log(actionJSONPayload.actions[0]["value"])
+  console.log("------------------") 
   console.log(actionJSONPayload.actions[0].value)
   if(actionJSONPayload.actions[0].value == "Delete"){
     deletePoll(actionJSONPayload);
