3dbd6411573104677952ea75c93409b17d4e0016
woobottle <dkrnfls@gmail.com>
2020-04-06 01:04:45 +0900

    danger 삭제

--- index.js
+++ index.js
@@ -122,27 +122,17 @@ app.post("/", urlencodedParser, function(req, res) {
       },
       {
         type: "actions",
-        // elements: [{
-        //     type: "button",
-        //     text: {
-        //       type: "plain_text",
-        //       emoji: true,
-        //       text: "항목추가하기(예정)"
-        //     },
-        //     style: "primary",
-        //     value: "Add"
-        //   },
-        //   {
-        //     type: "button",
-        //     text: {
-        //       type: "plain_text",
-        //       emoji: true,
-        //       text: "Delete"
-        //     },
-        //     style: "danger",
-        //     value: "Delete"
-        //   }
-        // ]
+        elements: [{
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "항목추가하기(예정)"
+            },
+            style: "primary",
+            value: "Add"
+          }
+        ]
       }
     ]
   };
@@ -299,27 +289,17 @@ function update_message(e){
     },
     {
       "type": "actions",
-      // "elements": [{
-      //     "type": "button",
-      //     "text": {
-      //       "type": "plain_text",
-      //       "emoji": true,
-      //       "text": "항목추가하기(예정)"
-      //     },
-      //     "style": "primary",
-      //     "value": "Add"
-      //   },
-      //   {
-      //     "type": "button",
-      //     "text": {
-      //       "type": "plain_text",
-      //       "emoji": true,
-      //       "text": "Delete"
-      //     },
-      //     "style": "danger",
-      //     "value": "Delete"
-      //   }
-      // ]
+      "elements": [{
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "emoji": true,
+            "text": "항목추가하기(예정)"
+          },
+          "style": "primary",
+          "value": "Add"
+        }
+      ]
     }
   ]
   var options = {
