593e2b793fb11242f116b933dda6b0b874fd83e9
woobottle <dkrnfls@gmail.com>
2020-04-06 00:47:35 +0900

    vote3 수정

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.post("/", urlencodedParser, function(req, res) {
           text: {
             type: "plain_text",
             emoji: true,
-            text: "Vote 3"
+            text: "Vote"
           },
           value: "vote_for_two"
         }
